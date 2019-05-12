b5e970508867a2cf0721789477db6085a0341c32
rishabhgupta <rishabhgupta@msn.com>
2019-05-12 17:59:22 +0530

    Updates

---
+++ precache-manifest.bf87acef77c4b1351b2773c367e132a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11724df05eeb22f4a326b24c5a445034",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c2c5b1efad8d5145abb",
+    "url": "/static/css/2.52738377.chunk.css"
+  },
+  {
+    "revision": "abd2b87363ca82063773",
+    "url": "/static/css/main.a5ac4182.chunk.css"
+  },
+  {
+    "revision": "2c2c5b1efad8d5145abb",
+    "url": "/static/js/2.eda5ee70.chunk.js"
+  },
+  {
+    "revision": "abd2b87363ca82063773",
+    "url": "/static/js/main.0eda6ac2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
